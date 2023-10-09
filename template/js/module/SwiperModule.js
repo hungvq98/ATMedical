ee7eeba255e5376cefc88209dc399f99a7950255
hungvq98 <hung.vq98@gmail.com>
2023-10-09 09:17:52 +0700

    Co-authored-by: Hy Hý <tronghyt1@gmail.com>

--- template/js/module/SwiperModule.js
+++ template/js/module/SwiperModule.js
@@ -61,116 +61,38 @@ export default function SwiperModule() {
         },
     })
 
-    var swiperBlogMain = new Swiper(".blog-slider-main .swiper", {
-        slidesPerView: "auto",
-        spaceBetween: 0,
-        effect: "fade",
-        pagination: {
-            el: ".blog-slider-main .swiper-pagination",
-            clickable: true,
-        },
-    });
-    var swiperBlogThumb = new Swiper(".blog-slider-thumb .swiper", {
-        slidesPerView: "auto",
-        spaceBetween: 0,
-        effect: "fade",
-        navigation: {
-            prevEl: ".blog-slider-thumb .swiper-prev",
-            nextEl: ".blog-slider-thumb .swiper-next",
-        }
-    });
+    // var swiperBlogMain = new Swiper(".blog-slider-main .swiper", {
+    //     slidesPerView: "auto",
+    //     spaceBetween: 0,
+    //     effect: "fade",
+    //     pagination: {
+    //         el: ".blog-slider-main .swiper-pagination",
+    //         clickable: true,
+    //     },
+    // });
+    // var swiperBlogThumb = new Swiper(".blog-slider-thumb .swiper", {
+    //     slidesPerView: "auto",
+    //     spaceBetween: 0,
+    //     effect: "fade",
+    //     navigation: {
+    //         prevEl: ".blog-slider-thumb .swiper-prev",
+    //         nextEl: ".blog-slider-thumb .swiper-next",
+    //     }
+    // });
 
-    swiperBlogMain.controller.control = swiperBlogThumb;
-    swiperBlogThumb.controller.control = swiperBlogMain;
+    // swiperBlogMain.controller.control = swiperBlogThumb;
+    // swiperBlogThumb.controller.control = swiperBlogMain;
 
-
-    functionSlider('.blog-dt-slider', {
+    functionSlider('.prod-slider', {
         speed: 1200,
         autoplay: {
             delaY: 5000,
         },
-        // slidesPerGroup: 2,
         initialSlide: 0,
         centeredSlides: false,
         loop: false,
         spaceBetween: 0,
         effect: "slide",
-        slidesPerView: "auto",
+        slidesPerView:'auto',
     })
-
-
-
-    var swiperProdThumb = new Swiper(".prod-slider-thumb .swiper", {
-        slidesPerView: "auto",
-        spaceBetween: 10,
-        direction: "vertical",
-        breakpoints: {
-            0:{
-                direction: "horizontal",
-            },
-            901: {
-                direction: "vertical",
-            }
-        }
-    });
-    var swiperProdMain = new Swiper(".prod-slider-main .swiper", {
-        slidesPerView: "auto",
-        spaceBetween: 0,
-        navigation: {
-            prevEl: ".prod-slider-thumb .swiper-prev",
-            nextEl: ".prod-slider-thumb .swiper-next",
-        },
-        thumbs: {
-            swiper: swiperProdThumb,
-        }
-    });
-
-
-    functionSlider('.prod-like', {
-        speed: 1200,
-        autoplay: {
-            delaY: 5000,
-        },
-        // slidesPerGroup: 2,
-        initialSlide: 0,
-        centeredSlides: false,
-        loop: false,
-        spaceBetween: 0,
-        effect: "slide",
-        slidesPerView:"auto",
-    })
-
-
-    const swColor = document.querySelectorAll(".homes-bn");
-    swColor.forEach(items => {
-        const swiperColor = items.querySelector('.swiper')
-        const progressPagi = items.querySelector('.progress')
-        const prev = items.querySelector('.swiper-prev')
-        const next = items.querySelector('.swiper-next')
-
-        var swiperColors = new Swiper(swiperColor, {
-            grabCursor: true,
-            navigation: {
-                prevEl:prev,
-                nextEl:next,
-            },
-            on: {
-                init: function (e) {
-                    const totalSlide = swiperColor.querySelectorAll(".swiper-slide")
-
-                    let progress = 100 / (totalSlide.length);
-                    let percent = (e.realIndex + 1) * progress;
-                    progressPagi.style = `--progress: ${percent}%`
-                },
-                slideChange: function (e) {
-                    const totalSlide = swiperColor.querySelectorAll(".swiper-slide")
-                    let progress = 100 / (totalSlide.length);
-                    let percent = (e.realIndex + 1) * progress;
-                    progressPagi.style = `--progress: ${percent}%`
-                },
-            }
-        });
-    })
-
-
 }
