a2d74d21b21f2688bbf66516ac493f1c965bd9c3
yoyaldev <yoyaldev@gmail.com>
2023-10-11 17:52:13 +0700

    update-code-diep

--- template/js/module/OpenClosePopup.js
+++ template/js/module/OpenClosePopup.js
@@ -1,5 +1,4 @@
 
-
 export default function OpenClosePopup() {
     try {
         const popup1 = document.getElementById("openpopup1");
@@ -7,21 +6,27 @@ export default function OpenClosePopup() {
             document.querySelector(".chitiet-tuyendung-overlay").classList.toggle("chitiet-tuyendung-open");
             document.querySelector(".popup-nophoso").classList.toggle("chitiet-tuyendung-open");
         });
-        
+
         const popup = document.getElementById("openpopup");
         popup.addEventListener("click", function () {
             document.querySelector(".chitiet-tuyendung-overlay").classList.toggle("chitiet-tuyendung-open");
             document.querySelector(".popup-nophoso").classList.toggle("chitiet-tuyendung-open");
         });
-        
+
         const closePopup = document.getElementById("closepopup");
         closePopup.addEventListener("click", function () {
             document.querySelector(".chitiet-tuyendung-overlay").classList.remove("chitiet-tuyendung-open");
             document.querySelector(".popup-nophoso").classList.remove("chitiet-tuyendung-open");
         });
-        
+
+        const closePopup1 = document.getElementById("overlay-close");
+        closePopup1.addEventListener("click", function () {
+            document.querySelector(".chitiet-tuyendung-overlay").classList.remove("chitiet-tuyendung-open");
+            document.querySelector(".popup-nophoso").classList.remove("chitiet-tuyendung-open");
+        });
+
     } catch (error) {
         console.log("Error:  " + error)
     }
-    
+
 }
